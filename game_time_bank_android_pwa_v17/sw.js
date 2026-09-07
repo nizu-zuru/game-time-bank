20157950bc1f4978cd7e88ff331d8582b2ae4d70
nizu-zuru <ago210ware@gmail.com>
2026-09-07 17:52:58 +0900

    Delete game_time_bank_android_pwa_v17 directory

--- game_time_bank_android_pwa_v17/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="game-time-bank-v12";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
